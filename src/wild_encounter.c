e767b21c219ef9ab2fad1fdd6485266f89b999ed
Lavender <42317903+LavenderG@users.noreply.github.com>
2020-04-15 23:01:32 +0200

    Make synchronize work 100% of the time.

--- src/wild_encounter.c
+++ src/wild_encounter.c
@@ -336,8 +336,7 @@ static u8 PickWildMonNature(void)
     }
     // check synchronize for a pokemon with the same ability
     if (!GetMonData(&gPlayerParty[0], MON_DATA_SANITY_IS_EGG)
-        && GetMonAbility(&gPlayerParty[0]) == ABILITY_SYNCHRONIZE
-        && Random() % 2 == 0)
+        && GetMonAbility(&gPlayerParty[0]) == ABILITY_SYNCHRONIZE)
     {
         return GetMonData(&gPlayerParty[0], MON_DATA_PERSONALITY) % 25;
     }
