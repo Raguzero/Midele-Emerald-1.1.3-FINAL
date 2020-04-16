8ca4e0b3d1f1e081f0a65840393b963d7b71635d
Lavender <42317903+LavenderG@users.noreply.github.com>
2020-04-16 15:29:02 +0200

    Castform with Pokérus + Pokérus chance 200/65536

--- src/script_pokemon_util_80F87D8.c
+++ src/script_pokemon_util_80F87D8.c
@@ -583,6 +583,11 @@ u8 ScriptGiveMon(u16 species, u8 level, u16 item, u32 unused1, u32 unused2, u8 u
     heldItem[0] = item;
     heldItem[1] = item >> 8;
     SetMonData(&mon, MON_DATA_HELD_ITEM, heldItem);
+    if (unused1 == 1) {
+        // Pokérus 4 días
+        u8 pokerus = 0xF4;
+        SetMonData(&mon, MON_DATA_POKERUS, &pokerus);
+    }
     sentToPc = GiveMonToPlayer(&mon);
     nationalDexNum = SpeciesToNationalPokedexNum(species);
 
