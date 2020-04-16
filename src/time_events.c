4f01660a2c56372b87479aa1a85566659eab1ede
Lavender <42317903+LavenderG@users.noreply.github.com>
2020-04-16 15:53:11 +0200

    Mirage Island always appears + Warp to Route 130 in debug room

--- src/time_events.c
+++ src/time_events.c
@@ -41,14 +41,7 @@ void UpdateMirageRnd(u16 days)
 
 bool8 IsMirageIslandPresent(void)
 {
-    u16 rnd = GetMirageRnd() >> 16;
-    int i;
-
-    for (i = 0; i < PARTY_SIZE; i++)
-        if (GetMonData(&gPlayerParty[i], MON_DATA_SPECIES) && (GetMonData(&gPlayerParty[i], MON_DATA_PERSONALITY) & 0xFFFF) == rnd)
-            return TRUE;
-
-    return FALSE;
+    return TRUE;
 }
 
 void UpdateShoalTideFlag(void)
