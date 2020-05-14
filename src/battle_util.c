0e4cdfb0537d017f1d6a57739267a93240bd710f
Raguzero <Shiyo_Wifi@hotmail.com>
2020-05-14 22:34:53 +0200

    Update Rough Skin to current gen + Snivy in Mauvile Game Corner

--- src/battle_util.c
+++ src/battle_util.c
@@ -2224,7 +2224,7 @@ u8 AbilityBattleEffects(u8 caseID, u8 battler, u8 ability, u8 special, u16 moveA
                  && TARGET_TURN_DAMAGED
                  && (gBattleMoves[move].flags & FLAG_MAKES_CONTACT))
                 {
-                    gBattleMoveDamage = gBattleMons[gBattlerAttacker].maxHP / 16;
+                    gBattleMoveDamage = gBattleMons[gBattlerAttacker].maxHP / 8;
                     if (gBattleMoveDamage == 0)
                         gBattleMoveDamage = 1;
                     BattleScriptPushCursor();
