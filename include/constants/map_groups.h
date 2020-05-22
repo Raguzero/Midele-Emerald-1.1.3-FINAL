d54bfaf2c2043037e9673b3656d64a4b5da86667
Lavender <42317903+LavenderG@users.noreply.github.com>
2020-05-22 20:46:28 +0200

    IzabeIsland_House2

--- include/constants/map_groups.h
+++ include/constants/map_groups.h
@@ -595,6 +595,7 @@
 #define MAP_IZABE_ISLAND_OUTSKIRTS (3 | (34 << 8))
 #define MAP_IZABE_ISLAND_TOWN      (4 | (34 << 8))
 #define MAP_IZABE_ISLAND_HOUSE1    (5 | (34 << 8))
+#define MAP_IZABE_ISLAND_HOUSE2    (6 | (34 << 8))
 
 #define MAP_GROUPS_COUNT 35
 
