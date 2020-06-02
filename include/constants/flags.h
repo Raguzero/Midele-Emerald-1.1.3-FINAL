dc111180018539f95b43494049d2073ef652845f
Lavender <42317903+LavenderG@users.noreply.github.com>
2020-06-02 11:04:41 +0200

    Event for IzabeIsland_House2 (template).

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -510,7 +510,7 @@
 #define FLAG_DEFEATED_HO_OH                  0x1DC
 #define FLAG_DEFEATED_LUGIA                  0x1DD
 
-#define FLAG_UNUSED_0x1DE                    0x1DE // Unused Flag
+#define FLAG_RECEIVED_IZABE_ITEM_1           0x1DE // Previously unused Flag
 #define FLAG_UNUSED_0x1DF                    0x1DF // Unused Flag
 #define FLAG_UNUSED_0x1E0                    0x1E0 // Unused Flag
 #define FLAG_UNUSED_0x1E1                    0x1E1 // Unused Flag
