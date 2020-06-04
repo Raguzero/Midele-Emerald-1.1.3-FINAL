9b639cab5da25a752e2b19617d755a72aba65dd4
Lavender <42317903+LavenderG@users.noreply.github.com>
2020-06-04 18:24:29 +0200

    Hidden items and encounters for IzabeIsland_UnderwaterLake.

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -652,9 +652,9 @@
 
 #define FLAG_HIDDEN_ITEM_IZABE_ISLAND_TOWN_POISON_BARB       (FLAG_HIDDEN_ITEMS_START + 0x5F) // Previously unused Flag
 #define FLAG_HIDDEN_ITEM_IZABE_ISLAND_TOWN_SHARP_BEAK        (FLAG_HIDDEN_ITEMS_START + 0x60) // Previously unused Flag
-#define FLAG_HIDDEN_ITEM_IZABE_ISLAND_HOUSE_1_LEEK           (FLAG_HIDDEN_ITEMS_START + 0x60) // Previously unused Flag
-#define FLAG_UNUSED_0x267  0x267 // Unused Flag
-#define FLAG_UNUSED_0x268  0x268 // Unused Flag
+#define FLAG_HIDDEN_ITEM_IZABE_ISLAND_HOUSE_1_LEEK           (FLAG_HIDDEN_ITEMS_START + 0x61) // Previously unused Flag
+#define FLAG_HIDDEN_ITEM_IZABE_ISLAND_LAKE_1  (FLAG_HIDDEN_ITEMS_START + 0x62) // Previously unused Flag
+#define FLAG_HIDDEN_ITEM_IZABE_ISLAND_LAKE_2  (FLAG_HIDDEN_ITEMS_START + 0x63) // Previously unused Flag
 #define FLAG_UNUSED_0x269  0x269 // Unused Flag
 #define FLAG_UNUSED_0x26A  0x26A // Unused Flag
 #define FLAG_UNUSED_0x26B  0x26B // Unused Flag
