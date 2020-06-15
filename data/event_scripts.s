8d2e23992f4a20c4097eee6dbbcec444813d69d5
Lavender <42317903+LavenderG@users.noreply.github.com>
2020-06-15 13:51:05 +0200

    Added map IzabeIsland_PokemonCenter_2F

--- data/event_scripts.s
+++ data/event_scripts.s
@@ -1093,3 +1093,5 @@ Common_EventScript_LegendaryFlewAway:: @ 8273776
 	.include "data/maps/IzabeIsland_OutskirtsCaveF1/scripts.inc"
 
 	.include "data/maps/IzabeIsland_PokemonCenter_1F/scripts.inc"
+
+	.include "data/maps/IzabeIsland_PokemonCenter_2F/scripts.inc"
