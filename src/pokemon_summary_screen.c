8fcf991867932378884444ffa5f202b7cb8b77c4
Raguzero <Shiyo_Wifi@hotmail.com>
2020-07-20 14:23:40 +0200

    Let a Pokémon forget any move they know (HMs)
    Credit to Lunos (https://www.pokecommunity.com/showthread.php?p=10182839#post10182839)

--- src/pokemon_summary_screen.c
+++ src/pokemon_summary_screen.c
@@ -2281,10 +2281,7 @@ static void HandleReplaceMoveInput(u8 taskId)
 
 static bool8 CanReplaceMove(void)
 {
-    if (sMonSummaryScreen->firstMoveIndex == MAX_MON_MOVES || sMonSummaryScreen->newMove == MOVE_NONE || IsMoveHm(sMonSummaryScreen->summary.moves[sMonSummaryScreen->firstMoveIndex]) != 1)
-        return TRUE;
-    else
-        return FALSE;
+    return TRUE;
 }
 
 static void ShowHMMovesCantBeForgottenWindow(u8 taskId)
