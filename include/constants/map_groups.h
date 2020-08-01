9d62313948b289a418ff490f7fe07e03d3e313f1
Raguzero <Shiyo_Wifi@hotmail.com>
2020-08-01 22:51:25 +0200

    Add new Shoal_Cave_Low_Tide map + Add SUICUNE wild event and respawn

--- include/constants/map_groups.h
+++ include/constants/map_groups.h
@@ -400,6 +400,7 @@
 #define MAP_SCORCHED_SLAB_1F                         (108 | (24 << 8))
 #define MAP_SCORCHED_SLAB_B2F                        (109 | (24 << 8))
 #define MAP_SCORCHED_SLAB_B3F                        (110 | (24 << 8))
+#define MAP_SHOAL_CAVE_LOW_SUICUNE                   (111 | (24 << 8))
 
 // Map Group 25
 #define MAP_SECRET_BASE_RED_CAVE1           (0 | (25 << 8))
