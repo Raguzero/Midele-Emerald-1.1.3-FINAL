451e74a793df637a9a171633de96be06768ccb88
Raguzero <Shiyo_Wifi@hotmail.com>
2020-09-12 21:33:19 +0200

    Fix bug Match Call with Party CUSTOM MIDELE
    Fix bug in Calls  (red death screen, nonexistent pokemon and names pokemon "??????" )

--- src/match_call.c
+++ src/match_call.c
@@ -1714,6 +1714,9 @@ static void PopulateSpeciesFromTrainerParty(int matchCallId, u8 *destStr)
     case F_TRAINER_PARTY_CUSTOM_MOVESET | F_TRAINER_PARTY_HELD_ITEM:
         speciesName = gSpeciesNames[party.ItemCustomMoves[monId].species];
         break;
+    case F_TRAINER_PARTY_CUSTOM_MIDELE:
+        speciesName = gSpeciesNames[party.ItemCustomMidele[monId].species];
+    break;
     }
 
     StringCopy(destStr, speciesName);
