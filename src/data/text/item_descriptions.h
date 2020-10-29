d09aca34c27ea2c6d4f529d2f4b2896c32c3ac3d
LavenderG <42317903+LavenderG@users.noreply.github.com>
2020-10-29 09:35:14 +0100

    Removed SOAR.
    SOAR caused Pokénav graphic bug in ROUTES.

--- src/data/text/item_descriptions.h
+++ src/data/text/item_descriptions.h
@@ -1648,8 +1648,3 @@ static const u8 sOldSeaMapDesc[] = _(
     "A faded sea chart\n"
     "that shows the way\n"
     "to a certain island.");
-
-static const u8 sEonFluteDesc[] = _(
-	"A flute that can\n"
-	"summon LATIAS or\n"
-	"LATIOS.");
