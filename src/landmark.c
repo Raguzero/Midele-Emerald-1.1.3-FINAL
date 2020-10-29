6bd0498a01c737d61fb5277bff0bd84ac8c95845
LavenderG <42317903+LavenderG@users.noreply.github.com>
2020-10-29 09:51:08 +0100

    OLDALE RUINS: Added map landmark

--- src/landmark.c
+++ src/landmark.c
@@ -57,6 +57,7 @@ static const u8 LandmarkName_MirageTower[] = _("MIRAGE TOWER");
 static const u8 LandmarkName_AlteringCave[] = _("ALTERING CAVE");
 static const u8 LandmarkName_DesertUnderpass[] = _("DESERT UNDERPASS");
 static const u8 LandmarkName_TrainerHill[] = _("TRAINER HILL");
+static const u8 LandmarkName_OldaleRuins[] = _("OLDALE RUINS");
 
 static const struct Landmark Landmark_FlowerShop = {LandmarkName_FlowerShop, FLAG_LANDMARK_FLOWER_SHOP};
 static const struct Landmark Landmark_PetalburgWoods = {LandmarkName_PetalburgWoods, -1};
@@ -99,6 +100,13 @@ static const struct Landmark Landmark_MirageTower = {LandmarkName_MirageTower, F
 static const struct Landmark Landmark_AlteringCave = {LandmarkName_AlteringCave, FLAG_LANDMARK_ALTERING_CAVE};
 static const struct Landmark Landmark_DesertUnderpass = {LandmarkName_DesertUnderpass, FLAG_LANDMARK_DESERT_UNDERPASS};
 static const struct Landmark Landmark_TrainerHill = {LandmarkName_TrainerHill, FLAG_LANDMARK_TRAINER_HILL};
+static const struct Landmark Landmark_OldaleRuins = {LandmarkName_OldaleRuins, FLAG_LANDMARK_OLDALE_RUINS};
+
+static const struct Landmark *const Landmarks_Route103_1[]  =
+{
+    &Landmark_OldaleRuins,
+    NULL,
+};
 
 static const struct Landmark *const Landmarks_Route103_2[]  =
 {
@@ -338,6 +346,7 @@ static const struct Landmark *const Landmarks_MtChimney_2[]  =
 
 static const struct LandmarkList gLandmarkLists[] =
 {
+    {MAPSEC_ROUTE_103, 1, Landmarks_Route103_1},
     {MAPSEC_ROUTE_103, 2, Landmarks_Route103_2},
     {MAPSEC_ROUTE_104, 0, Landmarks_Route104_0},
     {MAPSEC_ROUTE_104, 1, Landmarks_Route104_1},
