ae1fa1a8b61402c0b476134caa68a9988b24c122
Raguzero <Shiyo_Wifi@hotmail.com>
2020-11-18 19:32:08 +0100

    Enemy Mon with Trick works in RandomBattle CC + Music to more Trainers

--- src/battle_tower.c
+++ src/battle_tower.c
@@ -2182,14 +2182,14 @@ void DoSpecialTrainerBattle(void)
 	    case SPECIAL_RANDOM_BATTLE:
         gBattleTypeFlags = BATTLE_TYPE_TRAINER;
 		CreateTask(Task_StartBattleAfterTransition, 1);
-        PlayMapChosenOrBattleBGM(0);
+        PlayMapChosenOrBattleBGM(509);
         BattleTransition_StartOnField(sub_80B100C(6));
 		gTrainerBattleOpponent_A = TRAINER_RANDOM_PARTY;
 		break;
 		case SPECIAL_RANDOM_BATTLE_2:
         gBattleTypeFlags = BATTLE_TYPE_TRAINER;
 		CreateTask(Task_StartBattleAfterTransition, 1);
-        PlayMapChosenOrBattleBGM(0);
+        PlayMapChosenOrBattleBGM(509);
         BattleTransition_StartOnField(sub_80B100C(6));
 		gTrainerBattleOpponent_A = TRAINER_RANDOM_PARTY_2;
 		break;
