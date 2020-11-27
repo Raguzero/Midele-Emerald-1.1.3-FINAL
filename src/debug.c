0c0dfdc3dfbab1dc7471696cccce6e3eaab6d866
TheXaman <maxvdl@web.de>
2020-11-28 00:51:38 +0100

    now closes debug windows after giving all TMs

--- src/debug.c
+++ src/debug.c
@@ -1540,6 +1540,8 @@ static void DebugAction_Give_AllTMs(u8 taskId)
     for (i = ITEM_TM01; i <= ITEM_TM50; i++)
         if(!CheckBagHasItem(i, 1))
             AddBagItem(i, 1);
+    Debug_DestroyMenu(taskId);
+    EnableBothScriptContexts();
 }
 
 //Pokemon
