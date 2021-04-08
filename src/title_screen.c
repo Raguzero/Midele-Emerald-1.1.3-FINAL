dcb58f8b6c32861a66f28d23a65d136ef8d240bf
Lavender <42317903+LavenderG@users.noreply.github.com>
2021-04-08 17:29:57 +0200

    Enable the Reset RTC Feature
    https://github.com/pret/pokeemerald/wiki/Enable-the-Reset-RTC-Feature

--- src/title_screen.c
+++ src/title_screen.c
@@ -732,8 +732,7 @@ static void Task_TitleScreenPhase3(u8 taskId)
     {
         SetMainCallback2(CB2_GoToClearSaveDataScreen);
     }
-    else if ((gMain.heldKeys & RESET_RTC_BUTTON_COMBO) == RESET_RTC_BUTTON_COMBO
-      && CanResetRTC() == TRUE)
+    else if ((gMain.heldKeys & RESET_RTC_BUTTON_COMBO) == RESET_RTC_BUTTON_COMBO)
     {
         FadeOutBGM(4);
         BeginNormalPaletteFade(0xFFFFFFFF, 0, 0, 0x10, RGB_BLACK);
