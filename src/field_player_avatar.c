d13c4a48f3d187ae86ff16adde9f187ff396a8ec
Raguzero <Shiyo_Wifi@hotmail.com>
2021-07-07 14:49:39 +0200

    More speed in underwater maps

--- src/field_player_avatar.c
+++ src/field_player_avatar.c
@@ -676,6 +676,10 @@ static void PlayerNotOnBikeMoving(u8 direction, u16 heldKeys)
             PlayerGoSpeed1(direction);
         }
     }
+	else if (gPlayerAvatar.flags & PLAYER_AVATAR_FLAG_UNDERWATER)
+	{
+		PlayerGoSpeed2(direction);
+	}
     else
     {
         PlayerGoSpeed1(direction);
