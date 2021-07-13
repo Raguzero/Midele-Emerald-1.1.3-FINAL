ee9242071b77c060ebacf2af945988f451ccb939
Raguzero <Shiyo_Wifi@hotmail.com>
2021-07-13 13:26:10 +0200

    Add new music titlescreen

--- src/title_screen.c
+++ src/title_screen.c
@@ -604,7 +604,7 @@ void CB2_InitTitleScreen(void)
                                     | DISPCNT_OBJ_ON
                                     | DISPCNT_WIN0_ON
                                     | DISPCNT_OBJWIN_ON);
-        m4aSongNumStart(MUS_TITLE3);
+        m4aSongNumStart(CRUELANGELSTHESIS);
         gMain.state = 5;
         break;
     case 5:
