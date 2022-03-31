faf7af40bc60ab343d45825fc9ef03d31ee8f352
Manectric-Rang <Manectric-Rang@users.noreply.github.com>
2022-03-31 18:22:20 +0200

    Added flag for Metro battles

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -1392,7 +1392,7 @@
 #define FLAG_NURSE_UNION_ROOM_REMINDER              (SYSTEM_FLAGS + 0x20)
 
 #define FLAG_SYS_DEXNAV_SEARCH                      (SYSTEM_FLAGS + 0x21) // Previously unused Flag
-#define FLAG_UNUSED_0x882                           (SYSTEM_FLAGS + 0x22) // Unused Flag
+#define FLAG_RYU_RANDOMBATTLECCMETRO                (SYSTEM_FLAGS + 0x22) // Previously unused Flag
 #define FLAG_UNUSED_0x883                           (SYSTEM_FLAGS + 0x23) // Unused Flag
 #define FLAG_SYS_DAYLIGHT_SAVING                    (SYSTEM_FLAGS + 0x24)
 #define FLAG_UNUSED_0x885                           (SYSTEM_FLAGS + 0x25) // Unused Flag
